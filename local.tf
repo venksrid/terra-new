d1df6c093c31a0c13a861e8b8e3bcb14395df215
venkata-jonnalagadda <93720852+venkata-jonnalagadda@users.noreply.github.com>
2022-08-23 00:19:25 -0400

    update new

--- local.tf
+++ local.tf
@@ -1,6 +1,6 @@
 locals {
   bucket_name             = "testbucket0011100" # "origin-s3-bucket-${random_pet.this.id}"
-  destination_bucket_name = "replica-${local.bucket_name}-${random_pet.this.id}"
+  destination_bucket_name = "${local.bucket_name}-replica"
   origin_region           = "us-east-1"
   replica_region          = "us-west-2"
 
